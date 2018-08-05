549b7e9e94b2c97075ef8cde313ac2aba72a7a77
Craig Derington <craig@craigderington.me>
2018-08-05 08:17:04 -0400

    Update models.py - Fix naming convention for auto_trans_date

--- models.py
+++ models.py
@@ -134,7 +134,7 @@ class AppendedVisitor(Base):
     ppm_type = Column(String(10))
     ppm_indicator = Column(String(10))
     ppm_segment = Column(String(50))
-    auto_transaction_date = Column(String(50))
+    auto_trans_date = Column(String(50))
     last_seen = Column(String(50))
     birth_year = Column(Integer)
     income_range = Column(String(50))
